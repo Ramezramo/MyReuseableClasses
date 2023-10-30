cbf2ac029ddf8fc1704f89cf8edc95f3b160f2ef
Ramezramo <ramzmlak40@gmail.com>
2023-10-30 03:29:14 +0200

    adding the files tree

---
+++ lib/lib/main.dart
@@ -0,0 +1,26 @@
+// import 'package:bookly_app_with_tharwat/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'Features/Splash/presentation/views/splash_view.dart';
+import 'constants.dart';
+
+void main() {
+  runApp(const Bookly());
+}
+
+class Bookly extends StatelessWidget {
+  const Bookly({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor , 
+      textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+      ),
+      home: const SplashView(),
+    );
+  }
+}
