66ada542dc5e26406bfff0116257e93dcbca005b
Ramezramo <ramzmlak40@gmail.com>
2023-11-02 20:03:50 +0200

    configured the new project folder shape

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
-// import 'package:bookly_app_with_tharwat/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
-
 import 'Features/Splash/presentation/views/splash_view.dart';
 import 'constants.dart';
+
 
 void main() {
   runApp(const appName());
 }
+
 
 class appName extends StatelessWidget {
   const appName({Key? key}) : super(key: key);
